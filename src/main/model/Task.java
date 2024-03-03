2baaaf629232186f5feacc451364fe9650327475
Сидоров Иван <sid.i744@yandex.ru>
2024-03-03 21:38:44 +0300

    Исправить ошибки

--- src/main/model/Task.java
+++ src/main/model/Task.java
@@ -11,22 +11,7 @@ public class Task {
        this.title = title;
        this.discription = discription;
    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Task task = (Task) o;
-        return Objects.equals(id, task.id) &&
-                Objects.equals(status, task.status) &&
-                Objects.equals(discription, task.discription) &&
-                Objects.equals(title, task.title);
-    }
 
-    @Override
-    public int hashCode() {
-        // вызываем вспомогательный метод и передаём в него нужные поля
-        return Objects.hash(id, status, discription, title);
-    }
     @Override
     public String toString() {
         return title;
