fec5c66054db870b1e361e76b5e055ece8a8c390
Сидоров Иван <sid.i744@yandex.ru>
2024-05-12 02:56:59 +0300

    Исправить checkstyle

--- src/main/controllers/Managers.java
+++ src/main/controllers/Managers.java
@@ -2,15 +2,12 @@ package main.controllers;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
+import main.adapter.DurationAdapter;
+import main.adapter.LocalDateTimeAdapter;
 
 import java.time.Duration;
-import java.time.format.DateTimeFormatter;
 
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class Managers {
@@ -31,30 +28,7 @@ public class Managers {
         return gsonBuilder.create();
     }
 }
-class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
-    @Override
-    public void write(final JsonWriter jsonWriter, final LocalDateTime localDate) throws IOException {
-        jsonWriter.value(localDate.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-    }
 
-    @Override
-    public LocalDateTime read(final JsonReader jsonReader) throws IOException {
-        return LocalDateTime.parse(jsonReader.nextString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-    }
-}
-class DurationAdapter extends TypeAdapter<Duration> {
-
-    @Override
-    public void write(final JsonWriter jsonWriter, final Duration duration) throws IOException {
-        jsonWriter.value(duration.toString());
-    }
-    @Override
-    public Duration read(final JsonReader jsonReader) throws IOException {
-        return Duration.parse(jsonReader.nextString());
-    }
-
-
-}
 
 
 
